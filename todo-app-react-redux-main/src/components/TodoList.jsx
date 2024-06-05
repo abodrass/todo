a5c53f276e4279f4a95606f394cf39a1578f672b
hamzaaboras <hamzaaboras@hotmial.com>
2024-06-05 14:16:47 +0300

    my first commit

---
+++ todo-app-react-redux-main/src/components/TodoList.jsx
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  const filteredTodos = useSelector((state) => {
+    const todos = state.todos;
+    const filter = state.filter;
+    const searchTerm = state.searchTerm.toLowerCase(); // Convert search term to lowercase for case-insensitive search
+
+    return todos.filter((todo) => {
+      const matchesFilter = (filter === 'COMPLETED' && todo.completed) ||
+        (filter === 'INCOMPLETE' && !todo.completed) ||
+        filter === 'ALL';
+
+      const matchesSearch = todo.text.toLowerCase().includes(searchTerm);
+
+      return matchesFilter && matchesSearch;
+    });
+  });
+
+  console.log('Filtered Todos:', filteredTodos);
+
+  return (
+    <>
+    <p className="my-2 text-sm italic ">All Your Notes Here...</p>
+    <ul className='grid grid-cols-2'>
+      
+      {filteredTodos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} index={index} />
+      ))}
+    </ul>
+    </>
+  );
+};
+
+export default TodoList;
